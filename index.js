61227e5cf0582642366be5664ddbb9d8715d1999
Terrance Rose Jr <trose618@gmail.com>
2018-09-30 15:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function takeANumber(line, name){
   line.push(name)
-  //pos = line.length
   return `Welcome ${name}. You are number ${line.length} in line.`
 }
-var array = []
-console.log(takeANumber(array,"Terrance"))
-console.log(takeANumber(array,"Ilyasah"))
