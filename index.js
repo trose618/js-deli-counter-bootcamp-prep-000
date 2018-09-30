7b89421adbc8af9c01a223230b73728908925bd8
Terrance Rose Jr <trose618@gmail.com>
2018-09-30 15:55:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function currentLine(line){
   if(line.length===0){
     return "The line is currently empty."
   }
-  let theLine = "The line is currently: "
-  for(let i = 0; i < line.length;i++){
-    theLine = theLine + ""
+  let theLine = `The line is currently: 1. ${line[0]}`
+  for(let i = 1; i < line.length;i++){
+    theLine = theLine + `, ${i}. ${line[i]} `
   }
 }
 
