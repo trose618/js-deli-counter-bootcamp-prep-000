6808f17653f3a80e96206384038931c95864070c
Terrance Rose Jr <trose618@gmail.com>
2018-09-30 16:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(line){
   if(line.length===0){
     return "There is nobody waiting to be served!"
   }
-  return `Now serving ${line.shift()}`
+  return `Now serving ${line.shift()}.`
 }
 
 function currentLine(line){
