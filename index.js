0732670a2cacc20eafb353e32d77af8da4d8c1d5
Terrance Rose Jr <trose618@gmail.com>
2018-09-30 15:48:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function nowServing(line){
   return line.shift()
 }
 
+function currentLine(line){
+  if(line.length===0){
+    return "The line is currently empty."
+  }
+  let theLine = ""
+  for(let i = 0; i < line.length;i++){
+    
+  }
+}
 
